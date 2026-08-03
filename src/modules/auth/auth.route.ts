d85feda840e851e845957a9b3dcfde3073b5166e
shihabsumon585 <shihabsumon585@gmail.com>
2026-08-03 21:13:25 +0600

    edit auth route

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -6,5 +6,6 @@ import { authController } from "./auth.controller";
 const router = Router();
 
 router.post("/login", authController.authLogin)
+router.post("/login", authController.authLogin)
 
 export const authRoute = router;
